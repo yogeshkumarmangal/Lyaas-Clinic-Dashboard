799420a4a805bcca1588191ff294682902a5b4fd
Yogesh Kumar Mangal <42840043+yogeshkumarmangal@users.noreply.github.com>
2021-09-03 21:15:19 +0530

    Update apps.py

--- apps.py
+++ apps.py
@@ -13,7 +13,8 @@ colors = {
     'background': '#C26000 ',
     'text': '#7FDBFF'
 }
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets,meta_tags=[{'name': 'viewport',
+                #             'content': 'width=device-width, initial-scale=1.0, maximum-scale=1.2, minimum-scale=0.5,'}])
 server=app.server
 dbx = dropbox.Dropbox("5uSdWA0gd2UAAAAAAAAAAauPVaO_t_nlwRgP3YzwZ8-2HlxYFWRLUrmTAgk4F4b7")
 for entry in dbx.files_list_folder('').entries:
